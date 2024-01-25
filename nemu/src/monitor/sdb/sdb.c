34e9835dea942ffa55ac21ee34716c23d8aa6653
WenZhengkai <2441990178@qq.com>
2024-01-25 16:30:33 +0800

    sdb_mainloop() line 129, directly input q without error information

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -125,7 +125,9 @@ void sdb_mainloop() {
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }
+        if (cmd_table[i].handler(args) < 0) { 
+	nemu_state.state = nemu_state.state == NEMU_STOP ? NEMU_QUIT : nemu_state.state;	
+		return; }
         break;
       }
     }
