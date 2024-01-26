516a0aa059f85a3321b149b1f8dc47fb547f8d47
WenZhengkai <2441990178@qq.com>
2024-01-26 20:17:53 +0800

    finish the function of excute once(si) in sdb

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -54,6 +54,8 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);
+
 static struct {
   const char *name;
   const char *description;
@@ -62,7 +64,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si", "Excute the program once", cmd_si},
   /* TODO: Add more commands */
 
 };
@@ -90,6 +92,14 @@ static int cmd_help(char *args) {
     printf("Unknown command '%s'\n", arg);
   }
   return 0;
+}
+//PA1
+static int cmd_si(char *args){
+ 	char *arg = strtok(args, " ");
+	int n;
+  	arg == NULL? n = 1: sscanf(arg, "%d", &n);
+	cpu_exec(n);	
+	return 0;
 }
 
 void sdb_set_batch_mode() {
