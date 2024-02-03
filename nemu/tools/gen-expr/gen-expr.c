4467504e825394f1106aae4fa0d6f4022a39f69a
WenZhengkai <2441990178@qq.com>
2024-02-03 08:08:52 +0800

    generate the experession but can not ignore '1/0'

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -25,44 +25,49 @@ static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
+"#include <stdint.h>\n"
+"#pragma GCC diagnostic push\n"
+"#pragma GCC diagnostic ignored \"-fwrapv\"\n"
 "int main() { "
-"  unsigned result = %s; "
+"  uint32_t result = %s; "
 "  printf(\"%%u\", result); "
 "  return 0; "
-"}";
+"}\n"
+"#pragma GCC diagnostic pop\n";
 //PA1
-void gen(char c){
-	strcat(buf,&c);
+void gen(char* c){
+	strcat(buf,c);
 }
 //PA1
 void gen_num(){
-	uint32_t num = rand() % 1000 + 1;	
-	char *temp = NULL;
+	uint32_t num = rand() % 4294967295+ 1;	
+	char temp[11] = {};
 	sprintf(temp,"%u",num);
 	strcat(buf,temp);
 }
 //PA1
 void gen_rand_op(){
 	uint32_t num = rand() % 4 + 1;
-	char op;
+	char op[2];
 	switch(num){
-		case 1: op = '+';break;
-		case 2: op = '-';break;
-		case 3: op = '*';break;
-		case 4: op = '/';break;	
+		case 1:strcpy( op ,"+");break;
+		case 2:strcpy( op ,"-");break;
+		case 3:strcpy( op ,"*");break;
+		case 4:strcpy( op ,"/");break;	
 		default: assert(0);
 	}
-	strcat(buf,&op);
+	strcat(buf,op);
 }
 uint32_t choose(uint32_t n){
-	uint32_t num = rand() % (n - 2) + 0;
+	uint32_t num = rand() % n + 0;
 	return num;
 }
 static void gen_rand_expr() {
-  buf[0] = '\0';
-  switch (choose(3)){
+  //buf[0] = '\0';
+  switch ((strlen(buf)>8000)?0:choose(4)){
   	case 0: gen_num(); break;
-	case 1: gen('('); gen_rand_expr(); gen(')'); break;
+	case 1: gen("("); gen_rand_expr(); gen(")"); break;
+	case 2: gen(" ");gen_rand_expr();break;
 	default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
   }
 }
@@ -76,8 +81,9 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    strcpy(buf,"\0");
     gen_rand_expr();
-
+    
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
