9694e7eba5483e91cb845f4ac417e32a83860db0
WenZhengkai <2441990178@qq.com>
2024-07-28 11:06:17 +0800

    support npc to execute cmd_q

---
+++ npc/sim/include/macro.h
@@ -0,0 +1,8 @@
+#ifndef __MACRO_H__
+#define __MACRO_H__
+#include <string.h>
+
+// length of an array
+#define ARRLEN(arr) (int)(sizeof(arr) / sizeof(arr[0]))
+
+#endif
--- npc/sim/sdb/sdb.c
+++ npc/sim/sdb/sdb.c
@@ -1,8 +1,12 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
+#include "../include/macro.h"
 
 /* We use the 'readline' library to proviede more flexibility to read from strdin. */
+struct sdb_stru{
+	bool end;
+};
 static char* rl_gets() {
 	static char *line_read = NULL;
 	
@@ -20,12 +24,57 @@ static char* rl_gets() {
 	return line_read;
 }
 
-void sdb_mainloop(){
+static int cmd_q (char *args) {
+	return -1;
+}
+
+static struct {
+	const char *name;
+	const char *description;
+	int (*handler) (char *);
+} cmd_table [] = {
+	{"q", "Exit NPC simulation", cmd_q},
+
+	/* TODO: Add more commands */
+};
+
+#define NR_CMD ARRLEN(cmd_table)
+
+void sdb_mainloop(sdb_stru *sdb_info){
 	// batch_mode
 	
 	for(char *str; (str = rl_gets()) !=NULL;){
-	
+		char *str_end = str + strlen(str);
+
+		/* extract the first token as the command */
+		char *cmd = strtok(str, " ");
+		if (cmd == NULL) {continue; }
+
+		/* treat the remaining sting as the arguments,
+		 * witch may need further parsing
+		 */
+		char *args = cmd + strlen(cmd) + 1;
+		if (args >= str_end){
+			args = NULL;
+		}
+#ifdef CONFIG_DEVICE
+#endif
+		/* test for tokens */
+		printf("cmd: %s\nargs: %s\n", cmd, args);
+
+		int i;
+		for (i = 0; i < NR_CMD; i ++) {
+			if(strcmp(cmd, cmd_table[i].name) == 0) {
+				printf("cmd_table:%s\n",cmd);
+				if(cmd_table[i].handler(args) < 0) {
+				/* TODO: state transfer*/
+					sdb_info->end = true;
+					return;
+				}
+				break;
+			}
+		}
+		printf("NR_CMD:%d\n",NR_CMD);
+		if (i == NR_CMD) {printf("Unknown command '%s'\n", cmd);}
 	}
-
-
 }
