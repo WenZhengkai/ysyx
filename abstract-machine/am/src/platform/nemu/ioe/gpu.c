3149ef31dbd0407f8f945150f33b29e8c977f32a
WenZhengkai <2441990178@qq.com>
2024-12-13 22:16:22 +0800

    nemu rv32e, implement am:gpu and nemu:vga could display in am-test:video, but not correct  On branch pa2  Changes to be committed: 	modified:   abstract-machine/am/src/platform/nemu/ioe/gpu.c 	modified:   nemu/src/device/vga.c

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,20 +1,37 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+/*
+  int i;
+  int w = 400;  // TODO: get the correct width
+  int h = 300;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
+  */
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  //printf("__am_gpu_config:width=%d\theight=%d\n",inw(VGACTL_ADDR),inw(VGACTL_ADDR + 2));
+  int w = inw(VGACTL_ADDR + 2);
+  int h = inw(VGACTL_ADDR);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = w, .height = h,
+    .vmemsz = w * h * 32
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  int w_mul_h = ctl->w * ctl->h;
+  for(int i = 0; i < w_mul_h; i ++) {
+    fb[i + ctl->x * ctl->y] = ((uint32_t *)(ctl->pixels))[i];
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
